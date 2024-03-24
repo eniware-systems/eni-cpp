c000297901a581f2ce7a95f6cc1b5e4156fd80fb
void <void@randomaccess.de>
2024-03-24 17:43:04 +0100

    add string utils

--- eni-common/eni/strings.cpp
+++ eni-common/eni/strings.cpp
@@ -2,22 +2,204 @@
 // Created by void on 14/07/2021.
 //
 
-#include "strings.h"
+#include <algorithm>
+#include <eni/strings.h>
+
+#include <codecvt>
 
 namespace eni::strings {
 
 std::wstring toWide(const std::string &str) {
     using convert_typeX = std::codecvt_utf8<wchar_t>;
-    std::wstring_convert<convert_typeX, wchar_t> converterX;
+    std::wstring_convert<convert_typeX> converterX;
 
     return converterX.from_bytes(str);
 }
 
 std::string fromWide(const std::wstring &wideStr) {
     using convert_typeX = std::codecvt_utf8<wchar_t>;
-    std::wstring_convert<convert_typeX, wchar_t> converterX;
+    std::wstring_convert<convert_typeX> converterX;
 
     return converterX.to_bytes(wideStr);
+}
+
+void trim(std::string &str, const std::string &chars) {
+    auto from = str.find_first_not_of(chars);
+    auto to = str.find_last_not_of(chars);
+    if (to != std::string::npos) {
+        str.erase(to + 1, str.length() - 1);
+    }
+    if (from != std::string::npos) {
+        str.erase(0, from);
+    }
+}
+
+void trim(std::wstring &str, const std::wstring &chars) {
+    auto from = str.find_first_not_of(chars);
+    auto to = str.find_last_not_of(chars);
+
+    if (from == std::wstring::npos && to == std::wstring::npos) {
+        str.clear();
+    }
+
+    if (to != std::wstring::npos && to < str.length() - 1) {
+        str.erase(to + 1, str.length() - 1);
+    }
+    if (from != std::wstring::npos) {
+        str.erase(0, from);
+    }
+}
+
+std::vector<std::string> tokenize(const std::string &str,
+                                  const std::vector<std::string> &separators, const bool trim,
+                                  const bool skipEmpty, const bool keepSeparators) {
+    std::vector<std::wstring> separatorsW;
+
+    separatorsW.reserve(separators.size());
+    for (const auto &separator : separators) {
+        separatorsW.push_back(toWide(separator));
+    }
+
+    auto wr = tokenize(toWide(str), separatorsW, trim, skipEmpty,
+                       keepSeparators);
+    std::vector<std::string> r;
+    r.reserve(wr.size());
+    for (auto &wstr : wr) {
+        r.push_back(fromWide(wstr));
+    }
+    return r;
+}
+
+std::vector<std::wstring> tokenize(const std::wstring &str,// NOLINT(*-function-cognitive-complexity)
+                                   const std::vector<std::wstring> &separators,
+                                   const bool trim,
+                                   const bool skipEmpty, const bool keepSeparators) {
+    // since find_first_of seeks for any char in the given string, we need to make
+    // substring checks here
+    std::wstring::size_type n = 0;
+    std::wstring::size_type offset = 0;
+    std::vector<std::wstring> r;
+
+    while (n < str.length()) {
+        bool found = false;
+        for (const auto &separator : separators) {
+            if (n + separator.length() >= str.length()) {
+                continue;
+            }
+            std::wstring sub = str.substr(n, separator.length());
+            if (sub == separator) {
+                found = true;
+                std::wstring token = str.substr(offset, n - offset);
+                if (trim) {
+                    strings::trim(token);
+                }
+                n += separator.length();
+                offset = n;
+                if (!skipEmpty || !token.empty()) {
+                    if (!token.empty() ||
+                        !r.empty()) {// Do not allow first token to be empty.
+                        r.push_back(token);
+                    }
+                }
+
+                if (keepSeparators) {
+                    r.push_back(sub);
+                }
+                break;
+            }
+        }
+        if (!found) {
+            ++n;
+        }
+    }
+
+    if (auto remaining = str.substr(offset); !remaining.empty()) {
+        if (trim) {
+            strings::trim(remaining);
+        }
+
+        r.push_back(remaining);
+    }
+
+    return r;
+}
+
+std::string toSnakeCase(const std::string &str) {
+    const auto tokens = tokenize(str, std::vector<std::string>({" ", "\t", "-", "_"}), true,
+                                 true, false);
+    auto s = implode<std::string>("_", tokens);
+    std::ranges::transform(s, s.begin(), ::tolower);
+    return s;
+}
+
+std::string toKebabCase(const std::string &str) {
+    const auto tokens = tokenize(str, std::vector<std::string>({" ", "\t", "-", "_"}), true,
+                                 true, false);
+    auto s = implode<std::string>("-", tokens);
+    std::ranges::transform(s, s.begin(), ::tolower);
+    return s;
+}
+
+std::string toCamelCase(const std::string &str, const bool capitalizeFirstLetter) {
+    auto tokens = tokenize(str, std::vector<std::string>({" ", "\t", "-", "_"}), true,
+                           true, false);
+    std::stringstream ss;
+    bool first = true;
+    for (auto &t : tokens) {
+        if (!first || capitalizeFirstLetter) {
+            ss << std::toupper(t[0]);
+            ss << t.substr(1);
+        } else /*if (first)*/ {
+            first = false;
+            ss << t;
+        }
+    }
+    return ss.str();
+}
+
+std::string toUppercase(const std::string &str) {
+    auto result = str;
+    std::ranges::transform(result, result.begin(), ::toupper);
+    return result;
+}
+
+std::string toLowercase(const std::string &str) {
+    auto result = str;
+    std::ranges::transform(result, result.begin(), ::tolower);
+    return result;
+}
+
+std::string join(const std::string &separator, const std::vector<std::string> &components) {
+    std::string result;
+    bool first = true;
+
+    for (const std::string &c : components) {
+        if (!first) {
+            result += separator;
+        } else {
+            first = false;
+        }
+
+        result += c;
+    }
+    return result;
+}
+
+std::wstring join(const std::wstring &separator,
+                  const std::vector<std::wstring> &components) {
+    std::wstring result;
+    bool first = true;
+
+    for (const std::wstring &c : components) {
+        if (!first) {
+            result += separator;
+        } else {
+            first = false;
+        }
+
+        result += c;
+    }
+    return result;
 }
 
 }// namespace eni::strings
--- eni-common/eni/strings.h
+++ eni-common/eni/strings.h
@@ -5,9 +5,10 @@
 #ifndef ENI_STRINGS_H
 #define ENI_STRINGS_H
 
-#include <codecvt>
 #include <locale>
+#include <sstream>
 #include <string>
+#include <vector>
 
 namespace eni::strings {
 
@@ -25,6 +26,182 @@ namespace eni::strings {
  * @return The converted string.
  */
 [[nodiscard]] std::string fromWide(const std::wstring &str);
+
+/**
+ * Trims a string by removing characters from its left and right.
+ * @param str The string to trim.
+ * @param chars The characters to remove.
+ */
+extern void trim(std::string &str, const std::string &chars = " \t\n\r");
+
+/**
+ * Trims a string by removing characters from its left and right.
+ * @param str The string to trim.
+ * @param chars The characters to remove.
+ */
+extern void trim(std::wstring &str, const std::wstring &chars = L" \t\n\r");
+
+/**
+ * Splits a string into tokens.
+ * @param str The string to tokenize.
+ * @param separators A list of separators.
+ * @param trim Whether to trim the tokens.
+ * @param skipEmpty Whether to skip empty tokens.
+ * @param keepSeparators Whether to keep the separators as tokens.
+ * @return A list of tokens.
+ */
+extern std::vector<std::string> tokenize(const std::string &str,
+                                         const std::vector<std::string> &separators,
+                                         bool trim = true, bool skipEmpty = true,
+                                         bool keepSeparators = false);
+
+/**
+ * Splits a string into tokens.
+ * @param str The string to tokenize.
+ * @param separators A list of separators.
+ * @param trim Whether to trim the tokens.
+ * @param skipEmpty Whether to skip empty tokens.
+ * @param keepSeparators Whether to keep the separators as tokens.
+ * @return A list of tokens.
+ */
+extern std::vector<std::wstring>
+tokenize(const std::wstring &str, const std::vector<std::wstring> &separators,
+         bool trim = true, bool skipEmpty = true,
+         bool keepSeparators = false);
+
+/**
+ * Splits a string into tokens.
+ * @param str The string to tokenize.
+ * @param separator The separator.
+ * @param trim Whether to trim the tokens.
+ * @param skip_empty Whether to skip empty tokens.
+ * @param keep_separators Whether to keep the separators as tokens.
+ * @return A list of tokens.
+ */
+inline std::vector<std::string> tokenize(const std::string &str, const std::string &separator,
+                                         bool trim = true, bool skip_empty = true,
+                                         bool keep_separators = false) {
+    return tokenize(str, std::vector({separator}), trim, skip_empty,
+                    keep_separators);
+}
+
+/**
+ * Splits a string into tokens.
+ * @param str The string to tokenize.
+ * @param separator The separator.
+ * @param trim Whether to trim the tokens.
+ * @param skip_empty Whether to skip empty tokens.
+ * @param keep_separators Whether to keep the separators as tokens.
+ * @return A list of tokens.
+ */
+inline std::vector<std::wstring>
+tokenize(const std::wstring &str, const std::wstring &separator, bool trim = true,
+         bool skip_empty = true, bool keep_separators = false) {
+    return tokenize(str, std::vector<std::wstring>({separator}), trim, skip_empty,
+                    keep_separators);
+}
+
+/**
+ * Joins a list of strings into a new string.
+ * @param separator The separator to put between the string components.
+ * @param components The components list.
+ * @return The joined string.
+ */
+extern std::string join(const std::string &separator,
+                        const std::vector<std::string> &components);
+
+/**
+ * Joins a list of strings into a new string.
+ * @param separator The separator to put between the string components.
+ * @param components The components list.
+ * @return The joined string.
+ */
+extern std::wstring join(const std::wstring &separator,
+                         const std::vector<std::wstring> &components);
+
+/**
+ * Converts a string to snake_case.
+ * @param str The string.
+ * @return The converted string.
+ */
+extern std::string toSnakeCase(const std::string &str);
+
+/**
+ * Converts a string to kebab-case.
+ * @param str The string.
+ * @return The converted string.
+ */
+extern std::string toKebabCase(const std::string &str);
+
+/**
+ * Converts a string to camelCase.
+ * @param str The string.
+ * @param capitalizeFirstLetter Whether to begin the string with a capital
+ * letter.
+ * @return The converted string.
+ */
+extern std::string toCamelCase(const std::string &str,
+                               bool capitalizeFirstLetter = false);
+
+/**
+ * Converts a string to upper case.
+ * @param str The string.
+ * @return The converted string.
+ */
+extern std::string toUppercase(const std::string &str);
+
+/**
+ * Converts a string to lower case.
+ * @param str The string.
+ * @return The converted string.
+ */
+extern std::string toLowercase(const std::string &str);
+
+/**
+ * Implodes a list of elements into a string.
+ * @tparam T The type of the elements to implode, must be string-convertible.
+ * @tparam ContainerT The container type, must support range-based-for loops.
+ * @param separator The separator to put between the imploded elements.
+ * @param container The container of elements.
+ * @return The imploded string.
+ */
+template<typename T, typename ContainerT = std::vector<T>>
+std::string implode(const std::string &separator, const ContainerT &container) {
+    std::stringstream ss;
+    bool first = true;
+    for (auto &s : container) {
+        if (!first) {
+            ss << separator;
+        } else {
+            first = false;
+        }
+        ss << s;
+    }
+    return ss.str();
+}
+
+/**
+ * Implodes a list of elements into a string.
+ * @tparam T The type of the elements to implode, must be string-convertible.
+ * @tparam ContainerT The container type, must support range-based-for loops.
+ * @param separator The separator to put between the imploded elements.
+ * @param container The container of elements.
+ * @return The imploded string.
+ */
+template<typename T, typename ContainerT = std::vector<T>>
+std::wstring implode(const std::wstring &separator, const ContainerT &container) {
+    std::wstringstream ss;
+    bool first = true;
+    for (auto &s : container) {
+        if (!first) {
+            ss << separator;
+        } else {
+            first = false;
+        }
+        ss << s;
+    }
+    return ss.str();
+}
 
 }// namespace eni::strings
 
